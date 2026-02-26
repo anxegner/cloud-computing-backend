3ec75b50aad5dd51d06ab3defcfd324da772eac8
anxegner <annika.egner@tha.de>
2026-02-26 14:50:24 +0200

    add different deployment methods and requirements

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # cloud-computing-backend
+
+current python version: python 3.14.3
+
+## Start app in deployment
+waitress-serve --host 127.0.0.1 main:app
+
+## Start app in development
+flask --app main run --debug 
+
+## with app.py
+just run py -m app.py
--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from flask import Flask
+from flask import request
+import pickle
+import sklearn
 
 app = Flask(__name__)
 
+with open("sentiment_model.plk", "wb") as file:
+    loaded_data = pickle.load(file)
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def health():
+    return {"message": "Hello, World!"}
+
+@app.route("/api", methods=["POST", "GET"])
+def sentiment_api():
+ 
+    data = request.get_json()
+    return {"sentence": "The sky looks great today!", "sentiment": "positive"}
